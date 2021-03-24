cc309b71a6c2fc741abeb69bee98dabc53b49756
Peter Phillips <19808076+PeterMPhillips@users.noreply.github.com>
2021-03-24 15:11:57 -0700

    Update variable names (#355)

--- test/helpers/cookbook.js
+++ test/helpers/cookbook.js
@@ -1,9 +1,24 @@
 const { encodeTransferFrom } = require('./encode.js')
 
-async function prepareFlashLoan(strategy, arbitrager, sellRouter, buyRouter, loanAmount, loanToken, pairToken) {
+async function prepareFlashLoan(
+	strategy,
+	arbitrager,
+	sellAdapter,
+	buyAdapter,
+	loanAmount,
+	loanToken,
+	pairToken
+) {
 	const calls = []
 	// Withdraw flash loan
-	calls.push(await encodeTransferFrom(loanToken, strategy.address, arbitrager.address, loanAmount))
+	calls.push(
+		await encodeTransferFrom(
+			loanToken,
+			strategy.address,
+			arbitrager.address,
+			loanAmount
+		)
+	)
 	// Arbitrage and return flash loan
 	calls.push(
 		await encodeArbitrageLoan(
@@ -12,21 +27,21 @@ async function prepareFlashLoan(strategy, arbitrager, sellRouter, buyRouter, loa
 			loanAmount,
 			loanToken.address,
 			pairToken.address,
-			sellRouter.address,
-			buyRouter.address
+			sellAdapter.address,
+			buyAdapter.address
 		)
 	)
 	return calls
 }
 
-async function encodeArbitrageLoan(arbitrager, lender, amount, loanToken, pairToken, sellRouter, buyRouter) {
+async function encodeArbitrageLoan(arbitrager, lender, amount, loanToken, pairToken, sellAdapter, buyAdapter) {
 	const arbitrageLoanEncoded = await arbitrager.interface.encodeFunctionData('arbitrageLoan', [
 		lender,
 		amount,
 		loanToken,
 		pairToken,
-		sellRouter,
-		buyRouter,
+		sellAdapter,
+		buyAdapter,
 	])
 	return { target: arbitrager.address, callData: arbitrageLoanEncoded, value: 0 }
 }
