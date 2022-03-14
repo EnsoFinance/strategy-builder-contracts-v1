6d82d129681a50d7092ba87200f64b85a8fa1a6e
George Carder <georgercarder@gmail.com>
2022-03-14 14:21:30 -0700

    make StakingAdapter more specific to EnsoStakingAdapter

---
+++ contracts/adapters/staking/EnsoStakingAdapter.sol
@@ -0,0 +1,87 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/SafeCast.sol";
+import "../../libraries/SafeERC20.sol";
+import "../BaseAdapter.sol";
+import "../../interfaces/IRewardsAdapter.sol";
+import "../../interfaces/IStaking.sol";
+import "../../interfaces/IERC1155Supply.sol";
+
+contract EnsoStakingAdapter is BaseAdapter, IRewardsAdapter {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    address public immutable staking;
+    address public immutable distributionToken;
+
+    address public immutable stakedToken;
+
+    uint256 public immutable distributionTokenScalar;
+
+    constructor(
+        address stakedToken_,
+        address staking_,
+        address distributionToken_,
+        address weth_,
+        uint256 distributionTokenScalar_
+    ) BaseAdapter(weth_) public {
+        staking = staking_;
+        distributionToken = distributionToken_;
+        stakedToken = stakedToken_; 
+        require(distributionTokenScalar_ > 0, "EnsoStakingAdapter: invalid scaled distribution scalar.");
+        distributionTokenScalar = distributionTokenScalar_;
+    }
+
+    // @notice Calculates the stakedToken minted by staking
+    function spotPrice(
+        uint256 amount,
+        address tokenIn,
+        address tokenOut
+    ) external view override returns (uint256) {
+        require(tokenIn != tokenOut, "spotPrice: tokens cannot match.");
+        require(tokenIn == stakedToken || tokenIn == distributionToken, "spotPrice: invalid `tokenIn`.");
+        require(tokenOut == stakedToken || tokenOut == distributionToken, "spotPrice: invalid `tokenOut`.");
+        return (tokenIn == stakedToken) ? amount.mul(distributionTokenScalar) : amount.div(distributionTokenScalar);
+    }
+  
+    // @dev: stakes and unstakes stakedToken on behalf of `to` 
+    function swap(
+        uint256 amount,
+        uint256 expected,
+        address tokenIn,
+        address tokenOut,
+        address from,
+        address to
+    ) public override {
+        require(tokenIn != tokenOut, "swap: tokens cannot match.");
+        if (tokenIn == stakedToken) {
+            require(tokenOut == distributionToken, "swap: invalid `tokenOut`.");
+            if (from != address(this))
+              IERC20(tokenIn).safeTransferFrom(from, address(this), amount);
+            uint256 toBalanceBefore = IERC20(tokenOut).balanceOf(to);
+            IStaking(staking).stakeFor(to, SafeCast.toUint128(amount)); // ignores `hoursToStake` TODO suggest removal from interface
+            uint256 toBalanceAfter = IERC20(tokenOut).balanceOf(to);
+            uint256 difference = toBalanceAfter.sub(toBalanceBefore);
+            require(difference >= expected, "swap: Insufficient tokenOut amount");
+        } else if (tokenIn == distributionToken) {
+            require(tokenOut == stakedToken, "swap: invalid `tokenOut`.");
+            if (from != address(this))
+                IERC20(tokenIn).safeTransferFrom(from, address(this), amount);
+            uint256 toBalanceBefore = IERC20(tokenOut).balanceOf(to);
+            IStaking(staking).unstakeFor(to);
+            uint256 toBalanceAfter = IERC20(tokenOut).balanceOf(to);
+            uint256 difference = toBalanceAfter.sub(toBalanceBefore);
+            require(difference >= expected, "swap: Insufficient tokenOut amount");
+        } else {
+            revert("swap: token not supported.");
+        }
+    }
+
+    // Intended to be called via delegateCall
+    function claim(address token) external override {
+        // FIXME need to work out `claimFor` etc.. 
+    }
+
+}
