afe561c7b8d2f04e769c5e50a533530d8eaf2003
George Carder <georgercarder@gmail.com>
2022-03-17 15:49:19 -0700

    StakingMock.sol advances clock of staking for testing

--- contracts/mocks/StakingMock.sol
+++ contracts/mocks/StakingMock.sol
@@ -2,11 +2,26 @@
 
 pragma solidity >=0.6.0;
 
-import "@enso/staking/src/Staking.sol";
+//import "@enso/staking/src/Staking.sol";
+
+import "./src/Staking.sol"; // debug
 
 // mock class using Staking 
 contract StakingMock is Staking {
 
-  constructor(address stakingToken) Staking(stakingToken) {}
+  uint32 public advanceBy;
+
+  constructor(address stakingToken) Staking(stakingToken) {
+      advanceBy = 1 hours;
+  }
+
+  function currentHour() public override view returns(uint32 hour) {
+    hour = super.currentHour();
+    hour = hour + advanceBy; // no safe math since this is internal test and `advanceBy` should be sensible
+  }
+
+  function advanceClock(uint32 advanceBy_) public {
+      advanceBy = advanceBy_;
+  }
 
 }
