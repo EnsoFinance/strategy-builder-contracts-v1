9dc88f76bccf3bfefa3a08ba36b9493c200c46d7
George Carder <georgercarder@gmail.com>
2022-03-21 12:33:21 -0700

    update StakedEsnoEstimator

--- contracts/oracles/estimators/StakedEnsoEstimator.sol
+++ contracts/oracles/estimators/StakedEnsoEstimator.sol
@@ -15,10 +15,7 @@ contract StakedEnsoEstimator is IEstimator {
     function estimateItem(uint256 balance, address token) public view override returns (int256) {
         IStakedEnso sEnso = IStakedEnso(token);
         IERC20 enso = IStakedEnso(token).enso();
-        /*
-         The Staking.unstakeFor flow is distributionToken -> stakedToken 
-           where the amount of received stakedToken being min(amount, userStakes[user].amount)+userRewards[user].owed, a function of the user, which is the Strategy.
-        */
-        return IOracle(msg.sender).estimateItem(balance, address(enso));
+        uint256 amount = balance.mul(uint256(sEnso.maxHours())).div(3); 
+        return IOracle(msg.sender).estimateItem(amount, address(enso));
     }
 }
