f38ec8c8892a12821afec43e195e763b68a7d459
George Carder <georgercarder@gmail.com>
2022-03-23 13:42:30 -0700

    update mock for production

--- contracts/mocks/StakingMock.sol
+++ contracts/mocks/StakingMock.sol
@@ -2,9 +2,7 @@
 
 pragma solidity >=0.6.0;
 
-import "./DELETE_DONT_COMMIT/Staking.sol";
-
-//import "@enso/staking/src/Staking.sol";
+import "@enso/staking/src/Staking.sol";
 
 // mock class using Staking 
 contract StakingMock is Staking {
