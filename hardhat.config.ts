f22e7abba3e02d9b6c56cb350fba16a25be8cfdc
Kyle Dewhurst <15720036+kyledewy@users.noreply.github.com>
2022-04-06 17:31:12 +0200

    add registry as 6.11 to max out compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -131,6 +131,15 @@ let config: HardhatUserConfig = {
 					},
 				},
 			},
+            {
+				version: '0.6.11',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 2000000,
+					},
+				},
+			},
 			{
 				version: '0.6.12',
 				settings: {
