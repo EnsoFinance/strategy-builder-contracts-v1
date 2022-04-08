472d7a7e0b468be2da3b868031509942ecba6950
George Carder <georgercarder@gmail.com>
2022-04-08 10:42:10 -0700

    bugfix: contract object needed to be defined independently

--- tasks/addContractsToDefenderAdmin.ts
+++ tasks/addContractsToDefenderAdmin.ts
@@ -16,14 +16,14 @@ task(ADD_CONTRACTS_TO_DEFENDER_ADMIN, "Add Contracts to Defender Admin", async (
     throw Error("addContractsToDefenderAdmin: API_KEY or API_SECRET undefined.")
   }
   const client = new AdminClient({apiKey: API_KEY, apiSecret: API_SECRET});
-  let contracts = [];
+  let contracts : Contract[] = [];
   let network : Network = 'mainnet';
   let address : Address = '';
-  let contract : Contract = {network: network, address: address, name: '', abi: ''};
   let importPath = '';
   // most of core contracts
   for (const [key, value] of Object.entries(mainnet)) {
       address = value as Address
+      let contract : Contract = {network: network, address: address, name: '', abi: ''};
       contract.address = address 
       contract.name = key
       importPath = 'artifacts/contracts/'+key+'.sol/'+key+'.json' 
@@ -36,6 +36,7 @@ task(ADD_CONTRACTS_TO_DEFENDER_ADMIN, "Add Contracts to Defender Admin", async (
   // hacks for registries
   for (const [key, value] of Object.entries(mainnet)) {
       address = value as Address
+      let contract : Contract = {network: network, address: address, name: '', abi: ''};
       contract.address = address 
       contract.name = key
       importPath = 'artifacts/contracts/oracles/registries/'+key+'.sol/'+key+'.json' 
@@ -48,6 +49,7 @@ task(ADD_CONTRACTS_TO_DEFENDER_ADMIN, "Add Contracts to Defender Admin", async (
   // hack for oracle
   for (const [key, value] of Object.entries(mainnet)) {
       address = value as Address
+      let contract : Contract = {network: network, address: address, name: '', abi: ''};
       contract.address = address 
       contract.name = key
       importPath = 'artifacts/contracts/oracles/'+key+'.sol/'+key+'.json' 
@@ -57,9 +59,8 @@ task(ADD_CONTRACTS_TO_DEFENDER_ADMIN, "Add Contracts to Defender Admin", async (
       contract.abi = JSON.stringify(imported.abi)
       contracts.push(contract);
   }
-
+  
   for (var i=0; i<contracts.length; i++) {
       let result = await client.addContract(contracts[i])
-      console.log(result)
   }
 });
