9cf3ba55bf361e1725c0019538ad41c6326b6fe9
Peter Phillips <github@pmphillips.ca>
2022-04-19 21:23:38 -0700

    contract support for liquidity migration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -67,7 +67,7 @@ function getNetworks(): NetworksUserConfig {
 		if (archiveNode)
 			networks.hardhat.forking = {
 				url: archiveNode,
-				blockNumber: 14617575,
+				blockNumber: 14619730,
 			}
 	}
 	if (mnemonic && infuraApiKey) {
