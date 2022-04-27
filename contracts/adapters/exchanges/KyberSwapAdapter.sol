1a96c58027db92afa37bdd1330ebbf5fc9172b92
Peter Phillips <19808076+PeterMPhillips@users.noreply.github.com>
2022-04-27 14:38:48 -0700

    Switch to DMM + tests (#75)

--- contracts/adapters/exchanges/KyberSwapAdapter.sol
+++ contracts/adapters/exchanges/KyberSwapAdapter.sol
@@ -3,19 +3,20 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "../../libraries/SafeERC20.sol";
+import "../../interfaces/kyber/IDMMFactory.sol";
+import "../../interfaces/kyber/IDMMRouter02.sol";
 import "../BaseAdapter.sol";
-import "../../interfaces/kyber/ISimpleKyberProxy.sol";
 
 contract KyberSwapAdapter is BaseAdapter {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    address public immutable kyberNetwork;
-    address public immutable kyberProxy;
+    IDMMFactory public immutable dmmFactory;
+    IDMMRouter02 public immutable dmmRouter;
 
-    constructor(address kyberNetwork_, address kyberProxy_, address weth_) public BaseAdapter(weth_) {
-        kyberNetwork = kyberNetwork_;
-        kyberProxy = kyberProxy_;
+    constructor(address dmmFactory_, address dmmRouter_, address weth_) public BaseAdapter(weth_) {
+        dmmFactory = IDMMFactory(dmmFactory_);
+        dmmRouter = IDMMRouter02(dmmRouter_);
     }
 
     /*
@@ -33,14 +34,29 @@ contract KyberSwapAdapter is BaseAdapter {
         address to
     ) public override {
         require(tokenIn != tokenOut, "Tokens cannot match");
+
         if (from != address(this)) {
             uint256 beforeBalance = IERC20(tokenIn).balanceOf(address(this));
             IERC20(tokenIn).safeTransferFrom(from, address(this), amount);
             uint256 afterBalance = IERC20(tokenIn).balanceOf(address(this));
             amount = afterBalance.sub(beforeBalance); //In case of transfer fees reducing amount
         }
-        IERC20(tokenIn).approve(kyberNetwork, amount);
-        uint256 received = ISimpleKyberProxy(kyberProxy).swapTokenToToken(IERC20(tokenIn), amount, IERC20(tokenOut), 0);
-        require(received >= expected, "Insufficient tokenOut amount");
+        // Set up pools path
+        address[] memory poolsPath = new address[](1);
+        poolsPath[0] = dmmFactory.getPools(IERC20(tokenIn), IERC20(tokenOut))[0];
+        // Set up tokens path
+        IERC20[] memory path = new IERC20[](2);
+        path[0] = IERC20(tokenIn);
+        path[1] = IERC20(tokenOut);
+        // Approve and swap
+        IERC20(tokenIn).safeApprove(address(dmmRouter), amount);
+        dmmRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            amount,
+            expected,
+            poolsPath,
+            path,
+            to,
+            block.timestamp
+        );
     }
 }
