92877d1121a0ca813d29300c22ed3388cc47cf90
Peter Phillips <github@pmphillips.ca>
2022-04-28 13:43:01 -0700

    cache leverage item percentage

--- test/aave-adapter.ts
+++ test/aave-adapter.ts
@@ -17,8 +17,8 @@ import {
 	deployLoopRouter,
 	deployFullRouter
 } from '../lib/deploy'
-import { DEFAULT_DEPOSIT_SLIPPAGE, MAINNET_ADDRESSES } from '../lib/constants'
-//import { displayBalances } from '../lib/logging'
+import { MAINNET_ADDRESSES } from '../lib/constants'
+import { displayBalances } from '../lib/logging'
 import ERC20 from '@uniswap/v2-periphery/build/ERC20.json'
 import WETH9 from '@uniswap/v2-periphery/build/WETH9.json'
 import UniswapV2Factory from '@uniswap/v2-core/build/UniswapV2Factory.json'
@@ -72,7 +72,7 @@ describe('AaveAdapter', function () {
 
 		await tokens.registerTokens(accounts[0], strategyFactory)
 		collateralToken = tokens.aWETH
-		collateralToken2 = tokens.aWBTC
+		collateralToken2 = tokens.aCRV
 
 		router = await deployFullRouter(accounts[0], addressProvider, controller, library)
 		await whitelist.connect(accounts[0]).approve(router.address)
@@ -102,7 +102,7 @@ describe('AaveAdapter', function () {
 			{ token: collateralToken2,
 				percentage: BigNumber.from(1000),
 				adapters: [uniswapAdapter.address, aaveV2Adapter.address],
-				path: [tokens.wbtc],
+				path: [tokens.crv],
 				cache: ethers.utils.defaultAbiCoder.encode(
 	        ['uint16'],
 	        [500] // Multiplier 50% (divisor = 1000). For calculating the amount to purchase based off of the percentage
@@ -113,12 +113,15 @@ describe('AaveAdapter', function () {
 				adapters: [aaveV2DebtAdapter.address, uniswapAdapter.address],
 				path: [tokens.usdc, tokens.weth], //ending in weth allows for a leverage feedback loop
 				cache: ethers.utils.defaultAbiCoder.encode(
-	        ['address[]'],
-	        [[collateralToken, collateralToken2]] //define what tokens you want to loop back on here
+	        ['tuple(address token, uint16 percentage)[]'],
+	        [[
+						{ token: collateralToken, percentage: 500 },
+						{ token: collateralToken2, percentage: 500 }
+					]] //define what tokens you want to loop back on here
 	      ),
 			}
 		]
-		
+
 		strategyItems = prepareStrategy(positions, uniswapAdapter.address)
 		const strategyState: InitialState = {
 			timelock: BigNumber.from(60),
@@ -216,30 +219,6 @@ describe('AaveAdapter', function () {
 		await expect(strategy.connect(accounts[1]).withdrawAll(amount)).to.be.revertedWith('Cannot withdraw debt')
 	})
 
-	it('Should withdraw ETH', async function () {
-		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
-		const amount = BigNumber.from('5000000000000000')
-		const ethBalanceBefore = await accounts[1].getBalance()
-		const tx = await controller.connect(accounts[1]).withdrawETH(strategy.address, router.address, amount, DEFAULT_DEPOSIT_SLIPPAGE, '0x')
-		const receipt = await tx.wait()
-		console.log('Gas Used: ', receipt.gasUsed.toString())
-		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
-		const ethBalanceAfter = await accounts[1].getBalance()
-		expect(ethBalanceAfter.gt(ethBalanceBefore)).to.equal(true)
-	})
-
-	it('Should withdraw WETH', async function () {
-		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
-		const amount = BigNumber.from('5000000000000000')
-		const wethBalanceBefore = await weth.balanceOf(accounts[1].address)
-		const tx = await controller.connect(accounts[1]).withdrawWETH(strategy.address, router.address, amount, DEFAULT_DEPOSIT_SLIPPAGE, '0x')
-		const receipt = await tx.wait()
-		console.log('Gas Used: ', receipt.gasUsed.toString())
-		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
-		const wethBalanceAfter = await weth.balanceOf(accounts[1].address)
-		expect(wethBalanceAfter.gt(wethBalanceBefore)).to.equal(true)
-	})
-
 	it('Should restructure', async function () {
 		const positions = [
 			{ token: collateralToken,
@@ -256,8 +235,11 @@ describe('AaveAdapter', function () {
 				adapters: [aaveV2DebtAdapter.address, uniswapAdapter.address],
 				path: [tokens.usdc, tokens.weth],
 				cache: ethers.utils.defaultAbiCoder.encode(
-	        ['address[]'],
-	        [[collateralToken, collateralToken2]] //Need to keep collateralToken2 in the cache in order to deleverage it
+					['tuple(address token, uint16 percentage)[]'],
+	        [[
+						{ token: collateralToken, percentage: 500 },
+						{ token: collateralToken2, percentage: 0 }
+					]] //Need to keep collateralToken2 in the cache in order to deleverage it
 	      ),
 			}
 		]
@@ -280,6 +262,30 @@ describe('AaveAdapter', function () {
 		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
 	})
 
+	it('Should withdraw ETH', async function () {
+		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
+		const amount = BigNumber.from('5000000000000000')
+		const ethBalanceBefore = await accounts[1].getBalance()
+		const tx = await controller.connect(accounts[1]).withdrawETH(strategy.address, router.address, amount, '990', '0x')
+		const receipt = await tx.wait()
+		console.log('Gas Used: ', receipt.gasUsed.toString())
+		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
+		const ethBalanceAfter = await accounts[1].getBalance()
+		expect(ethBalanceAfter.gt(ethBalanceBefore)).to.equal(true)
+	})
+
+	it('Should withdraw WETH', async function () {
+		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
+		const amount = BigNumber.from('5000000000000000')
+		const wethBalanceBefore = await weth.balanceOf(accounts[1].address)
+		const tx = await controller.connect(accounts[1]).withdrawWETH(strategy.address, router.address, amount, '990', '0x')
+		const receipt = await tx.wait()
+		console.log('Gas Used: ', receipt.gasUsed.toString())
+		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
+		const wethBalanceAfter = await weth.balanceOf(accounts[1].address)
+		expect(wethBalanceAfter.gt(wethBalanceBefore)).to.equal(true)
+	})
+
 	it('Should deploy new strategy', async function () {
 		const name = 'New Strategy'
 		const symbol = 'NEW'
@@ -291,7 +297,7 @@ describe('AaveAdapter', function () {
 				adapters: [aaveV2Adapter.address],
 				path: [],
 				cache: ethers.utils.defaultAbiCoder.encode(
-					["uint16"],
+					['uint16'],
 					[500], // Multiplier 50% (divisor = 1000). For calculating the amount to purchase based off of the percentage
 				),
 			},
@@ -300,7 +306,10 @@ describe('AaveAdapter', function () {
 				percentage: BigNumber.from(-1000),
 				adapters: [aaveV2DebtAdapter.address, uniswapAdapter.address],
 				path: [tokens.usdc, tokens.weth],
-				cache: ethers.utils.defaultAbiCoder.encode(["address[]"], [[tokens.aWETH]]),
+				cache: ethers.utils.defaultAbiCoder.encode(
+					['tuple(address token, uint16 percentage)[]'],
+					[[{ token: collateralToken, percentage: 500 }]] //define what tokens you want to loop back on here
+				)
 			}
 		]
 
@@ -375,14 +384,20 @@ describe('AaveAdapter', function () {
 				percentage: BigNumber.from(-500),
 				adapters: [aaveV2DebtAdapter.address, uniswapAdapter.address],
 				path: [tokens.usdc, tokens.weth],
-				cache: ethers.utils.defaultAbiCoder.encode(["address[]"], [[tokens.aWETH]]),
+				cache: ethers.utils.defaultAbiCoder.encode(
+					['tuple(address token, uint16 percentage)[]'],
+					[[{ token: collateralToken, percentage: 250 }]] //define what tokens you want to loop back on here),
+				)
 			},
 			{
 				token: tokens.debtWBTC,
 				percentage: BigNumber.from(-500),
 				adapters: [aaveV2DebtAdapter.address, uniswapAdapter.address],
 				path: [tokens.wbtc, tokens.weth],
-				cache: ethers.utils.defaultAbiCoder.encode(["address[]"], [[tokens.aWETH]]),
+				cache: ethers.utils.defaultAbiCoder.encode(
+					['tuple(address token, uint16 percentage)[]'],
+					[[{ token: collateralToken, percentage: 250 }]] //define what tokens you want to loop back on here),
+				)
 			}
 		]
 
@@ -437,14 +452,109 @@ describe('AaveAdapter', function () {
 		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
 	})
 
-	it('Should deploy debt meta strategy, that is unbalanced.', async function () {
+	it('Should deploy ambiguous strategy', async function () {
+		const name = 'Ambiguous Strategy'
+		const symbol = 'AMBI'
+
+		const positions = [
+				{ token: collateralToken,
+						percentage: BigNumber.from(500),
+						adapters: [aaveV2Adapter.address],
+						path: [],
+						cache: ethers.utils.defaultAbiCoder.encode(
+								['uint16'],
+								[500]
+						),
+				},
+				{ token: collateralToken2,
+						percentage: BigNumber.from(1500),
+						adapters: [uniswapAdapter.address, aaveV2Adapter.address],
+						path: [tokens.crv],
+						cache: ethers.utils.defaultAbiCoder.encode(
+								['uint16'],
+								[500]
+						),
+				},
+				{ token: tokens.debtUSDC,
+						percentage: BigNumber.from(-875),
+						adapters: [aaveV2DebtAdapter.address, uniswapAdapter.address],
+						path: [tokens.usdc, tokens.weth],
+						cache: ethers.utils.defaultAbiCoder.encode(
+								['tuple(address token, uint16 percentage)[]'],
+								[[
+									{ token: collateralToken, percentage: 250 },
+									{ token: collateralToken2, percentage: 500 }
+								]] //define what tokens you want to loop back on here
+						),
+				},
+				{ token: tokens.debtWBTC,
+						percentage: BigNumber.from(-125),
+						adapters: [aaveV2DebtAdapter.address, uniswapAdapter.address],
+						path: [tokens.wbtc, tokens.weth],
+						cache: ethers.utils.defaultAbiCoder.encode(
+								['tuple(address token, uint16 percentage)[]'],
+								[[{ token: collateralToken, percentage: 250 }]] //define what tokens you want to loop back on here
+						),
+				}
+		]
+
+		strategyItems = prepareStrategy(positions, uniswapAdapter.address)
+		const strategyState: InitialState = {
+			timelock: BigNumber.from(60),
+			rebalanceThreshold: BigNumber.from(50),
+			rebalanceSlippage: BigNumber.from(997),
+			restructureSlippage: BigNumber.from(980), // Restucturing from this strategy requires higher slippage tolerance
+			performanceFee: BigNumber.from(0),
+			social: false,
+			set: false
+		}
+
+		const tx = await strategyFactory
+		.connect(accounts[1])
+		.createStrategy(
+			accounts[1].address,
+			name,
+			symbol,
+			strategyItems,
+			strategyState,
+			router.address,
+			'0x',
+			{ value: WeiPerEther }
+		)
+		const receipt = await tx.wait()
+		console.log('Deployment Gas Used: ', receipt.gasUsed.toString())
+
+		const strategyAddress = receipt.events.find((ev: Event) => ev.event === 'NewStrategy').args.strategy
+		const Strategy = await getContractFactory('Strategy')
+
+		strategy = await Strategy.attach(strategyAddress)
+		const LibraryWrapper = await getContractFactory('LibraryWrapper', {
+			libraries: {
+				StrategyLibrary: library.address
+			}
+		})
+		wrapper = await LibraryWrapper.deploy(oracle.address, strategyAddress)
+		await wrapper.deployed()
+
+		await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
+		expect(await wrapper.isBalanced()).to.equal(true)
+	})
+
+	it('Should deposit', async function () {
+		const tx = await controller.connect(accounts[1]).deposit(strategy.address, router.address, 0, '990', '0x', { value: WeiPerEther })
+		const receipt = await tx.wait()
+		console.log('Deposit Gas Used: ', receipt.gasUsed.toString())
+		//await displayBalances(wrapper, strategyItems.map((item) => item.item), weth)
+	})
+
+	it('Should deploy debt meta strategy', async function () {
 		//await displayBalances(basicWrapper, basicStrategyItems.map((item) => item.item), weth)
-		
+
 		const loopRouter = await deployLoopRouter(accounts[0], controller, library)
 		await whitelist.connect(accounts[0]).approve(loopRouter.address)
 
-		let name = 'Test Strategy2'
-		let symbol = 'TEST2'
+		let name = 'Basic Strategy'
+		let symbol = 'BASIC'
 		let positions = [
 			{ token: weth.address, percentage: BigNumber.from(500) },
 			{ token: usdc.address, percentage: BigNumber.from(500) },
@@ -477,10 +587,10 @@ describe('AaveAdapter', function () {
 		name = 'Debt Meta Strategy'
 		symbol = 'DMETA'
 		let positions2 = [
-			{ token: basicStrategy.address, 
+			{ token: basicStrategy.address,
         percentage: BigNumber.from(400),
-        adapters: [metaStrategyAdapter.address], 
-        path: [] 
+        adapters: [metaStrategyAdapter.address],
+        path: []
       },
       { token: tokens.aWETH,
         percentage: BigNumber.from(1200),
@@ -496,12 +606,12 @@ describe('AaveAdapter', function () {
         adapters: [aaveV2DebtAdapter.address, uniswapAdapter.address],
         path: [tokens.usdc, weth.address], //ending in weth allows for a leverage feedback loop
         cache: ethers.utils.defaultAbiCoder.encode(
-          ['address[]'],
-          [[tokens.aWETH]] //define what tokens you want to loop back on here
+					['tuple(address token, uint16 percentage)[]'],
+					[[{ token: tokens.aWETH, percentage: 500 }]]
         ),
       },
 		]
-    
+
 		let metaStrategyItems = prepareStrategy(positions2, uniswapAdapter.address)
 		tx = await strategyFactory
 			.connect(accounts[1])
