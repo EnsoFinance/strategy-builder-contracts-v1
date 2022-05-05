3476e3c054faddd934de77dc137f08886b397af1
Peter Phillips <github@pmphillips.ca>
2022-05-05 08:21:19 -0700

    auto update trade data

--- scripts/crawl-strategy-adapters.ts
+++ scripts/crawl-strategy-adapters.ts
@@ -1,10 +1,15 @@
 import hre from "hardhat";
-import { Contract } from "ethers";
+import dotenv from "dotenv"
+import { Contract, Wallet } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import Strategy from '../artifacts/contracts/Strategy.sol/Strategy.json'
 import StrategyProxyFactory from '../artifacts/contracts/StrategyProxyFactory.sol/StrategyProxyFactory.json'
-//import { write2File} from "./common"
+import { waitForTransaction } from "./common"
 import deploymentsJSON from '../deployments.json'
+
+dotenv.config()
+
+let privateKey: string | undefined = process.env.PRIVATE_KEY
 
 const deployments: {[key: string]: {[key: string]: string}} = deploymentsJSON
 let contracts: {[key: string]: string} = {}
@@ -20,7 +25,7 @@ deprecated.map((deprecatedKey: string) => {
   addressMapping[contracts[deprecatedKey]] = contracts[contractKey]
 })
 
-async function crawlStrategies(factory: Contract, signer: SignerWithAddress) {
+async function crawlStrategies(factory: Contract, signer: SignerWithAddress | Wallet) {
     const filter = factory.filters.NewStrategy()
     const events = await factory.queryFilter(filter);
     let strategies = events.map((event) => {
@@ -34,8 +39,7 @@ async function crawlStrategies(factory: Contract, signer: SignerWithAddress) {
         const tradeData = await strategies[i].getTradeData(items[j])
         const intersection = tradeData.adapters.filter((adapter: string) => addressMapping[adapter] !== undefined)
         if (intersection.length > 0) {
-          console.log("\nUpdate strategy: ", strategies[i].address)
-          console.log("Strategy Manager", await strategies[i].manager())
+          const manager = await strategies[i].manager()
           const adapters = tradeData.adapters.map((adapter: string) => {
             const newAdapter = addressMapping[adapter]
             if (newAdapter) {
@@ -44,28 +48,51 @@ async function crawlStrategies(factory: Contract, signer: SignerWithAddress) {
               return adapter
             }
           })
+          console.log('\nStrategy: ', strategies[i].address)
           console.log('Old adapters: ', tradeData.adapters)
           console.log('New adapters: ', adapters)
-          const encoding = strategies[i].interface.encodeFunctionData('updateTradeData', [
-            items[j],
-            {
-             adapters: adapters,
-             path: tradeData.path,
-             cache: tradeData.cache
-            }
-          ])
-          console.log("\nEncoding: ", encoding)
+          if (signer.address == manager) {
+            console.log("\nUpdating strategy...")
+            await waitForTransaction(async (txArgs) => {
+              return strategies[i].updateTradeData(
+                items[j],
+                {
+                 adapters: adapters,
+                 path: tradeData.path,
+                 cache: tradeData.cache
+                },
+                txArgs
+              )
+            }, signer)
+            console.log("Success!")
+          } else {
+            console.log("\nUpdate needed!")
+            console.log("Strategy Manager", manager)
+            const encoding = strategies[i].interface.encodeFunctionData('updateTradeData', [
+              items[j],
+              {
+               adapters: adapters,
+               path: tradeData.path,
+               cache: tradeData.cache
+              }
+            ])
+            console.log("\nEncoding: ", encoding)
+          }
         }
       }
     }
 }
 
 async function main() {
-    const [ signer ] = await hre.ethers.getSigners();
-
-    const factory = new hre.ethers.Contract(contracts['StrategyProxyFactory'], StrategyProxyFactory.abi, signer);
-    await crawlStrategies(factory, signer);
-    //write2File("uni_v3_pairs.json", tokenlist);
+    //const [ signer ] = await hre.ethers.getSigners();
+    if (privateKey) {
+      const signer = new hre.ethers.Wallet(privateKey, hre.ethers.provider)
+      console.log("Signer: ", signer.address)
+      const factory = new hre.ethers.Contract(contracts['StrategyProxyFactory'], StrategyProxyFactory.abi, signer);
+      await crawlStrategies(factory, signer);
+    } else {
+      console.log("Private key not found!")
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
