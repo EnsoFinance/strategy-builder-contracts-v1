90b0068889a08ec54ff041ddce1f1f296f41b66a
George Carder <georgercarder@gmail.com>
2022-06-03 15:36:52 -0700

    reset errorIndex every fn

--- errors/errorMacro.sh
+++ errors/errorMacro.sh
@@ -27,6 +27,7 @@ BOOKMARK="BOOKMARK"
 applyMacros() {
 
   input="$1"
+  errorIndexer=0
 
   inputHash=$(echo "$input" | sha256sum)
   inputHash=${inputHash:0:12}
@@ -59,7 +60,7 @@ applyMacros() {
     isErrorMacroForLine=$(echo "$line" | grep "$error_macro_for")
     if [ "$isErrorMacroLine" != "" ]; then
       error="$inputHash"$(printf "%02x" "$errorIndexer")
-      # 2 chars for contract, 2 chars for errorIndexer
+      # chars for contract, 2 chars for errorIndexer
       errorToStore=$(echo "$line" | sed s/.*$ERROR_MACRO\(// | sed s/\).*//)
 
       jsonObject=$(echo $jsonObject | jq ".[$inputIndex].errorcodes.\"$error\" = $errorToStore")
@@ -68,7 +69,7 @@ applyMacros() {
       errorIndexer=$((errorIndexer+1))
     elif [ "$isErrorMacroForLine" != "" ]; then
       error="$inputHash"$(printf "%02x" "$errorIndexer")
-      # 2 chars for contract, 2 chars for errorIndexer
+      # chars for contract, 2 chars for errorIndexer
       errorToStore=$(echo "$line" | sed s/.*$error_macro_for\(// | sed s/\).*//)
       jsonObject=$(echo $jsonObject | jq ".[$inputIndex].errorcodes.\"$error\" = $errorToStore")
       line=$(echo "$line" | sed "s/uint256(0x[a-z0-9]\+) $commentStart $error_macro_for($errorToStore) $commentEnd/uint256(0x$error) $commentStart $error_macro_for($errorToStore) $commentEnd/") 
