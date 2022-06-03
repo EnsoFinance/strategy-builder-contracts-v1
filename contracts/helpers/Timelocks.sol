ce59c4f528e039442a6eed08adf0338a67b26ab1
George Carder <georgercarder@gmail.com>
2022-06-03 09:21:43 -0700

    introduce generic Timelocks contract since many contracts need it.

---
+++ contracts/helpers/Timelocks.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.6.12;
+
+contract Timelocks {
+    
+    mapping(bytes4 => TimelockData) internal _timelockData;
+    bytes32 constant public UNSET_VALUE = keccak256("Timelocks: unset value.");
+
+    struct TimelockData {
+        uint256 delay;
+        uint256 timestamp;
+        bytes value;
+    }
+
+    function _setTimelock(bytes4 selector, uint256 delay) internal {
+        TimelockData storage td = _timelockData[selector]; 
+        td.delay = delay;
+        td.value = abi.encode(UNSET_VALUE);
+    }
+
+    function _startTimelock(bytes4 selector, bytes memory value) internal {
+        TimelockData storage td = _timelockData[selector]; 
+        td.timestamp = block.timestamp;
+        td.value = value;
+    }
+
+    function _timelockIsReady(bytes4 selector) internal returns(bool) {
+        TimelockData memory td = _timelockData[selector]; 
+        if (td.timestamp == 0) return false;
+        if (td.timestamp >= block.timestamp + td.delay) return true;
+    }
+
+    // unchecked, assumes caller has checked `isReady`
+    function _getTimelockValue(bytes4 selector) internal returns(bytes memory) {
+        return _timelockData[selector].value; 
+    }
+
+    function _resetTimelock(bytes4 selector) internal {
+        TimelockData storage td = _timelockData[selector]; 
+        td.timestamp = 0;
+        td.value = abi.encode(UNSET_VALUE);
+    }
+}
