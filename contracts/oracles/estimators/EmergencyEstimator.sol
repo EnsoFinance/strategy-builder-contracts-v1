ba382069c592fd9319020c0a1513aca4a65393da
George Carder <georgercarder@gmail.com>
2022-06-06 10:23:34 -0700

    setEstimate with timelock and event

--- contracts/oracles/estimators/EmergencyEstimator.sol
+++ contracts/oracles/estimators/EmergencyEstimator.sol
@@ -6,18 +6,33 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "../../interfaces/IEstimator.sol";
 import "../../interfaces/IERC20NonStandard.sol";
+import "../../helpers/Timelocks.sol";
 
-contract EmergencyEstimator is IEstimator, Ownable {
+contract EmergencyEstimator is IEstimator, Ownable, Timelocks {
     using SignedSafeMath for int256;
 
     mapping(address => int256) public estimates;
+
+    event EstimateSet(address token, int256 amount, bool finalized);
+
+    constructor() public {
+        _setTimelock(this.setEstimate.selector, 5 minutes);
+    }
 
     function estimateItem(uint256 balance, address token) public view override returns (int256) {
         return _estimateItem(balance, token);
     }
 
     function setEstimate(address token, int256 amount) external onlyOwner {
+        _startTimelock(this.setEstimate.selector, abi.encode(token, amount));
+        emit EstimateSet(token, amount, false);
+    }
+
+    function finalizeSetEstimate() external {
+        require(_timelockIsReady(this.setEstimate.selector), "finalizeSetEstimate: timelock not ready.");
+        (address token, int256 amount) = abi.decode(_getTimelockValue(this.setEstimate.selector), (address, int256));
         estimates[token] = amount;
+        emit EstimateSet(token, amount, true);
     }
 
     function estimateItem(address user, address token) public view override returns (int256) { 
--- test/strategy-controller.ts
+++ test/strategy-controller.ts
@@ -10,6 +10,7 @@ import { BigNumber, Contract, Event } from 'ethers'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { prepareStrategy, Position, StrategyItem, InitialState } from '../lib/encode'
 import { DEFAULT_DEPOSIT_SLIPPAGE, ITEM_CATEGORY, ESTIMATOR_CATEGORY, TIMELOCK_CATEGORY } from '../lib/constants'
+import { increaseTime } from '../lib/utils'
 import { deployTokens, deployUniswapV2, deployUniswapV2Adapter, deployPlatform, deployLoopRouter, Platform } from '../lib/deploy'
 //import { displayBalances } from '../lib/logging'
 
@@ -786,6 +787,10 @@ describe('StrategyController', function () {
 		const EmergencyEstimator = await getContractFactory('EmergencyEstimator')
 		const emergencyEstimator = await EmergencyEstimator.attach(emergencyEstimatorAddress)
 		await emergencyEstimator.setEstimate(tokens[1].address, originalEstimate)
+
+		await increaseTime(10*60)
+
+		await emergencyEstimator.finalizeSetEstimate()
 
 		await expect(controller.connect(accounts[1]).deposit(strategy.address, router.address, 0, DEFAULT_DEPOSIT_SLIPPAGE, '0x', { value: WeiPerEther})).to.emit(controller, 'Deposit')
 	})
