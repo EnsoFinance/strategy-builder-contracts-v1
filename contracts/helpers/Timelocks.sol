d4e2cd92c312efa3c37fc19c3875fe64f168a6bb
George Carder <georgercarder@gmail.com>
2022-06-08 08:38:40 -0700

    make _timelockData virtual

--- contracts/helpers/Timelocks.sol
+++ contracts/helpers/Timelocks.sol
@@ -1,10 +1,11 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.6.12;
 
-contract Timelocks {
+abstract contract Timelocks {
     
-    mapping(bytes4 => TimelockData) internal _timelockData;
     bytes constant public UNSET_VALUE = abi.encode(keccak256("Timelocks: unset value."));
+
+    function _timelockData(bytes4 functionSelector) internal virtual returns(TimelockData storage);
 
     struct TimelockData {
         uint256 delay;
@@ -15,30 +16,30 @@ contract Timelocks {
     // delay value is not validated but is assumed to be sensible 
     // since this function is internal, this way `_timelockIsReady` will not overflow
     function _setTimelock(bytes4 selector, uint256 delay) internal {
-        TimelockData storage td = _timelockData[selector]; 
+        TimelockData storage td = _timelockData(selector); 
         td.delay = delay;
         td.value = UNSET_VALUE;
     }
 
     function _startTimelock(bytes4 selector, bytes memory value) internal {
-        TimelockData storage td = _timelockData[selector]; 
+        TimelockData storage td = _timelockData(selector); 
         td.timestamp = block.timestamp;
         td.value = value;
     }
 
     function _timelockIsReady(bytes4 selector) internal returns(bool) {
-        TimelockData memory td = _timelockData[selector]; 
+        TimelockData memory td = _timelockData(selector); 
         if (td.timestamp == 0) return false;
         if (block.timestamp >= td.timestamp + td.delay) return true;
     }
 
     // unchecked, assumes caller has checked `isReady`
     function _getTimelockValue(bytes4 selector) internal returns(bytes memory) {
-        return _timelockData[selector].value; 
+        return _timelockData(selector).value; 
     }
 
     function _resetTimelock(bytes4 selector) internal {
-        TimelockData storage td = _timelockData[selector]; 
+        TimelockData storage td = _timelockData(selector); 
         td.timestamp = 0;
         td.value = UNSET_VALUE;
     }
