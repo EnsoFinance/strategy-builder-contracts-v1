d7a9bdad4cce04918593d9e5f12d79b615d2cf35
George Carder <georgercarder@gmail.com>
2022-06-17 09:53:33 -0700

    outline: setStructure populates rewardsTokens efficiently

--- contracts/interfaces/registries/ITokenRegistry.sol
+++ contracts/interfaces/registries/ITokenRegistry.sol
@@ -1,7 +1,9 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.6.0 <0.9.0;
+pragma experimental ABIEncoderV2;
 
 import "../IEstimator.sol";
+import "../../helpers/StrategyTypes.sol";
 
 interface ITokenRegistry {
     function itemCategories(address token) external view returns (uint256);
@@ -10,13 +12,22 @@ interface ITokenRegistry {
 
     function estimators(uint256 categoryIndex) external view returns (IEstimator);
 
-    function claimable(address item) external view returns(bool);
+    struct ItemDetails {
+        bool isClaimable;
+        StrategyTypes.TradeData tradeData;
+    }
+
+    function itemDetails(address item) external view returns(ItemDetails memory);
+
+    function isClaimable(address item) external view returns(bool);
 
     function getEstimator(address token) external view returns (IEstimator);
 
     function addEstimator(uint256 estimatorCategoryIndex, address estimator) external;
 
-    function addItem(uint256 itemCategoryIndex, uint256 estimatorCategoryIndex, address token, bool isClaimable) external;
+    function addItem(uint256 itemCategoryIndex, uint256 estimatorCategoryIndex, address token) external;
 
-    function addItems(uint256[] calldata itemCategoryIndex, uint256[] calldata estimatorCategoryIndex, address[] calldata token, bool[] calldata claimable) external;
+    function addItemDetailed(uint256 itemCategoryIndex, uint256 estimatorCategoryIndex, address token, StrategyTypes.TradeData memory tradeData, bool isClaimable) external;
+
+    function addItems(uint256[] calldata itemCategoryIndex, uint256[] calldata estimatorCategoryIndex, address[] calldata token) external;
 }
--- contracts/oracles/registries/TokenRegistry.sol
+++ contracts/oracles/registries/TokenRegistry.sol
@@ -1,20 +1,30 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "../../interfaces/registries/ITokenRegistry.sol";
+import "../../helpers/StrategyTypes.sol";
 
 contract TokenRegistry is ITokenRegistry, Ownable {
     mapping(address => uint256) public override itemCategories;
     mapping(address => uint256) public override estimatorCategories;
     mapping(uint256 => IEstimator) public override estimators;
-    mapping(address => bool) public override claimable;
+    mapping(address => ItemDetails) private _itemDetails;
 
     event EstimatorAdded(address estimator, uint256 estimatorCategoryIndex);
-    event ItemAdded(address token, uint256 itemCategoryIndex, uint256 estimatorCategoryIndex, bool isClaimable);
+    event ItemAdded(address token, uint256 itemCategoryIndex, uint256 estimatorCategoryIndex);
 
     function getEstimator(address token) external view override returns (IEstimator) {
         return estimators[estimatorCategories[token]];
+    }
+
+    function itemDetails(address item) external view override returns(ItemDetails memory) {
+        return _itemDetails[item];
+    }
+
+    function isClaimable(address item) external view override returns(bool) {
+        return _itemDetails[item].isClaimable;
     }
 
     function addEstimator(uint256 estimatorCategoryIndex, address estimator) external override onlyOwner {
@@ -22,23 +32,29 @@ contract TokenRegistry is ITokenRegistry, Ownable {
         emit EstimatorAdded(estimator, estimatorCategoryIndex);
     }
 
-    function addItem(uint256 itemCategoryIndex, uint256 estimatorCategoryIndex, address token, bool isClaimable) external override onlyOwner {
-        _addItem(itemCategoryIndex, estimatorCategoryIndex, token, isClaimable);    
+    function addItem(uint256 itemCategoryIndex, uint256 estimatorCategoryIndex, address token) external override onlyOwner {
+        _addItem(itemCategoryIndex, estimatorCategoryIndex, token);  
     }
 
-    function addItems(uint256[] calldata itemCategoryIndex, uint256[] calldata estimatorCategoryIndex, address[] calldata token, bool[] calldata isClaimable) external override onlyOwner {
+    function addItemDetailed(uint256 itemCategoryIndex, uint256 estimatorCategoryIndex, address token, StrategyTypes.TradeData memory tradeData, bool isClaimable) external override onlyOwner {
+        ItemDetails storage id = _itemDetails[token];
+        id.isClaimable = isClaimable;
+        id.tradeData = tradeData;
+        _addItem(itemCategoryIndex, estimatorCategoryIndex, token);    
+    }
+
+    function addItems(uint256[] calldata itemCategoryIndex, uint256[] calldata estimatorCategoryIndex, address[] calldata token) external override onlyOwner {
         uint numItems = itemCategoryIndex.length; 
-        require(estimatorCategoryIndex.length == numItems && numItems == token.length && numItems == isClaimable.length, "Mismatched array lengths");
-        for (uint i = 0; i < numItems; i++) {
-           _addItem(itemCategoryIndex[i], estimatorCategoryIndex[i], token[i], isClaimable[i]); 
+        require(estimatorCategoryIndex.length == numItems && numItems == token.length, "Mismatched array lengths");
+        for (uint i; i < numItems; ++i) {
+           _addItem(itemCategoryIndex[i], estimatorCategoryIndex[i], token[i]);
         }
     }
 
-    function _addItem(uint256 itemCategoryIndex, uint256 estimatorCategoryIndex, address token, bool isClaimable) internal {
+    function _addItem(uint256 itemCategoryIndex, uint256 estimatorCategoryIndex, address token) internal {
         require(address(estimators[estimatorCategoryIndex]) != address(0), "Invalid category");
         itemCategories[token] = itemCategoryIndex;
         estimatorCategories[token] = estimatorCategoryIndex;
-        claimable[token] = isClaimable;
-        emit ItemAdded(token, itemCategoryIndex, estimatorCategoryIndex, isClaimable);
+        emit ItemAdded(token, itemCategoryIndex, estimatorCategoryIndex);
     }
 }
