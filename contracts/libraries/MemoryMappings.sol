8243b66d5e988d4ffcba15602db2da1aba71b170
George Carder <georgercarder@gmail.com>
2022-06-16 19:31:45 -0700

    optimize mapping append

--- contracts/libraries/MemoryMappings.sol
+++ contracts/libraries/MemoryMappings.sol
@@ -8,29 +8,22 @@ library MemoryMappings {
 
     function append(BinaryTreeWithPayload.Tree memory mm, bytes32 key, bytes32 value) internal pure returns(bool) {
         BinaryTreeWithPayload.Tree memory node = mm.get(uint256(key)); 
+        bytes32[] memory arr;
         if (!node.exists) {
-            mm.add(uint256(key), abi.encode(value));
+            arr = new bytes32[](1);
+            arr[0] = value;
+            mm.add(uint256(key), abi.encode(arr));
             return true; // isNew
         }
-        bytes memory payload = node.payload;
-        bytes32[] memory toStore;
-        if (payload.length > 32) { // is bytes32[] FIXME ??
-            (bytes32[] memory arr) = abi.decode(payload, (bytes32[]));    
-            toStore = new bytes32[](arr.length + 1);
-            // FIXME need more efficient copy/append
-            uint256 i;
-            for (; i < arr.length; ++i) { // FIXME use efficient append
-                toStore[i] = arr[i]; 
-            }
-            toStore[i] = value;
-        } else { // is bytes32
-            // FIXME optimize using assembly
-            (bytes32 singleton) = abi.decode(payload, (bytes32));
-            toStore = new bytes32[](2);
-            toStore[0] = singleton;
-            toStore[1] = value;
+        (arr) = abi.decode(node.payload, (bytes32[]));
+        assembly {
+            let bytesWritten := mload(arr)
+            bytesWritten := add(bytesWritten, 32)
+            mstore(arr, add(arr, and(add(add(bytesWritten, 0x20), 0x1f), not(0x1f))))
+            mstore(add(arr, add(mul(mload(arr), 32), 32)), value)
+            mstore(arr, bytesWritten)
         }
-        mm.add(uint256(key), abi.encode(toStore));
+        mm.add(uint256(key), abi.encode(arr));
     }
 
     function add(BinaryTreeWithPayload.Tree memory mm, bytes32 key, bytes32 value) internal pure {
