40df21c91798bdcb5ad2b4ab4848c42086e8e3aa
George Carder <georgercarder@gmail.com>
2022-06-16 19:53:08 -0700

    del fees for bytecode size

--- contracts/test/OtherStrategyToken.sol
+++ contracts/test/OtherStrategyToken.sol
@@ -229,7 +229,7 @@ abstract contract OtherStrategyToken is IStrategyToken, OtherStrategyTokenStorag
     ) internal virtual {
         _validAddress(sender);
         _validAddress(recipient);
-        _handleFees(amount, sender, recipient);
+        //_handleFees(amount, sender, recipient);
         _balances[sender] = _balances[sender].sub(amount, BALANCE_LOW);
         _balances[recipient] = _balances[recipient].add(amount);
         _resetTokenValue(sender);
