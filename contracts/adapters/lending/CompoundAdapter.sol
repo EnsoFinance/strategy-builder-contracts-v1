dd61facd97bd98416b1df29303ff4f7928adcf60
George Carder <georgercarder@gmail.com>
2022-06-20 15:20:22 -0700

    cleanup

--- contracts/adapters/lending/CompoundAdapter.sol
+++ contracts/adapters/lending/CompoundAdapter.sol
@@ -68,12 +68,4 @@ contract CompoundAdapter is ProtocolAdapter, IRewardsAdapter, StringUtils {
         ret[0] = comptroller.getCompAddress();
         return ret;
     }
-
-    // Intended to be called via delegateCall
-    /*function claim(address token) external override {
-        require(_checkToken(token), "Not claimable");
-        address[] memory tokens = new address[](1);
-        tokens[0] = token;
-        comptroller.claimComp(address(this), tokens);
-    }*/
 }
