9d6b7420b41d4627d5092f5f8d54931be60aff8e
George Carder <georgercarder@gmail.com>
2022-07-13 09:26:10 -0700

    math comment

--- contracts/StrategyTokenBase.sol
+++ contracts/StrategyTokenBase.sol
@@ -229,7 +229,14 @@ abstract contract StrategyTokenBase is IStrategyTokenBase, StrategyTokenStorage 
         address recipient,
         uint256 amount
     ) internal virtual {
-        // TODO comment with mathematical justification as to why this is secure
+        /*
+         we just check the product of addresses a * b since a * b = "0"
+         implies WLOG that either: 
+          - a==0 
+          - a!=0 and either b==0 or b is the unique zero-divisor of a
+            given such an a, the probability of finding the associated
+            non-zero zero-divisor b is 1 / (2**160 - 2) which is negligible
+        **/
         _validAddress(address(uint256(sender) * uint256(recipient)));
         _balances[sender] = _balances[sender].sub(amount, BALANCE_LOW);
         _balances[recipient] = _balances[recipient].add(amount);
