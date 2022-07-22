d1da07170c911bd5c56500b9a67f1d65c117a895
PeterMPhillips <github@pmphillips.ca>
2022-07-22 14:06:40 -0700

    add tests for aave adapters

--- test/live-estimates.ts
+++ test/live-estimates.ts
@@ -5,8 +5,8 @@ import { Estimator } from '../lib/estimator'
 import { Tokens } from '../lib/tokens'
 import { getLiveContracts } from '../lib/mainnet'
 import { increaseTime } from '../lib/utils'
-import { deployFullRouter, deployUniswapV3Adapter } from '../lib/deploy'
-import { DIVISOR, MAINNET_ADDRESSES } from '../lib/constants'
+import { deployFullRouter, deployUniswapV3Adapter, deployAaveV2Adapter, deployAaveV2DebtAdapter } from '../lib/deploy'
+import { DIVISOR, MAINNET_ADDRESSES, ESTIMATOR_CATEGORY } from '../lib/constants'
 import WETH9 from '@uniswap/v2-periphery/build/WETH9.json'
 
 import StrategyClaim from '../artifacts/contracts/libraries/StrategyClaim.sol/StrategyClaim.json'
@@ -30,8 +30,43 @@ describe('Live Estimates', function () {
 		eNFTP: Contract,
 		eETH2X: Contract,
 		strategyClaim: Contract,
-		oldUniswapV3Address: string,
-		newUniswapV3Address: string
+		oldAdapters: string[],
+		newAdapters: string[]
+
+	async function updateAdapters(strategy: Contract) {
+			// Impersonate manager
+			const managerAddress = await strategy.manager()
+			await network.provider.request({
+				method: 'hardhat_impersonateAccount',
+				params: [managerAddress],
+			})
+			const manager = await ethers.getSigner(managerAddress)
+
+			// Send funds to manager
+			await accounts[19].sendTransaction({to: managerAddress, value: WeiPerEther})
+
+			const items = await strategy.items()
+			for (let i = 0; i < items.length; i++) {
+					let tradeData = await strategy.getTradeData(items[i])
+					let adapters = [...tradeData.adapters]
+					let shouldUpdate = false;
+					for (let j = 0; j < adapters.length; j++) {
+					 	for (let k = 0; k < oldAdapters.length; k++) {
+								if (adapters[j].toLowerCase() == oldAdapters[k].toLowerCase()) {
+										adapters[j] = newAdapters[k];
+										shouldUpdate = true;
+										break;
+								}
+						}
+					}
+					if (shouldUpdate) {
+						 await strategy.connect(manager).updateTradeData(items[i], {
+							 ...tradeData,
+							 adapters: adapters
+						 })
+					}
+			}
+	}
 
 	before('Setup Uniswap + Factory', async function () {
 		accounts = await getSigners()
@@ -42,6 +77,9 @@ describe('Live Estimates', function () {
 			params: [ownerAddress],
 		})
 		const owner = await ethers.getSigner(ownerAddress)
+
+		// Send funds to owner
+		await accounts[19].sendTransaction({to: ownerAddress, value: WeiPerEther.mul(5)})
 
 		tokens = new Tokens()
 		weth = new Contract(tokens.weth, WETH9.abi, accounts[0])
@@ -54,6 +92,7 @@ describe('Live Estimates', function () {
 
 		let {
 			aaveV2,
+			aaveV2Debt,
 			compound,
 			curve,
 			curveLP,
@@ -67,12 +106,30 @@ describe('Live Estimates', function () {
 			yearnV2,
 		} = enso.adapters
 
-		oldUniswapV3Address = uniswapV3.address
-		// Deploy new uniswap v3 adapter
-		uniswapV3 = await deployUniswapV3Adapter(accounts[0], enso.platform.oracles.registries.uniswapV3Registry,  new Contract(MAINNET_ADDRESSES.UNISWAP_V3_ROUTER, [], accounts[0]), weth)
-		// Whitelist
+		// Store old adapter addresses
+		oldAdapters = [uniswapV3.address, aaveV2.address, aaveV2Debt.address]
+		// Deploy and whitelist new adapters
+		const uniswapV3Router = new Contract(MAINNET_ADDRESSES.UNISWAP_V3_ROUTER, [], owner)
+		const aaveAddressProvider = new Contract(MAINNET_ADDRESSES.AAVE_ADDRESS_PROVIDER, [], owner)
+		uniswapV3 = await deployUniswapV3Adapter(
+			owner, enso.platform.oracles.registries.uniswapV3Registry,
+			uniswapV3Router,
+			weth
+		)
 		await enso.platform.administration.whitelist.connect(owner).approve(uniswapV3.address)
-		newUniswapV3Address = uniswapV3.address
+		aaveV2 = await deployAaveV2Adapter(
+			accounts[0],
+			aaveAddressProvider,
+			enso.platform.controller,
+			weth,
+			enso.platform.oracles.registries.tokenRegistry,
+			ESTIMATOR_CATEGORY.AAVE_V2
+		)
+		await enso.platform.administration.whitelist.connect(owner).approve(aaveV2.address)
+		aaveV2Debt = await deployAaveV2DebtAdapter(owner, aaveAddressProvider, weth)
+		await enso.platform.administration.whitelist.connect(owner).approve(aaveV2Debt.address)
+		// Store new adapter addresses
+		newAdapters = [uniswapV3.address, aaveV2.address, aaveV2Debt.address]
 
 		estimator = new Estimator(
 			accounts[0],
@@ -105,6 +162,11 @@ describe('Live Estimates', function () {
 		eYLA = await Strategy.attach('0xb41a7a429c73aa68683da1389051893fe290f614')
 		eNFTP = await Strategy.attach('16f7a9c3449f9c67e8c7e8f30ae1ee5d7b8ed10d')
 		eETH2X = await Strategy.attach('0x81cddbf4a9d21cf52ef49bda5e5d5c4ae2e40b3e')
+		await updateAdapters(eDPI)
+		await updateAdapters(eYETI)
+		await updateAdapters(eYLA)
+		await updateAdapters(eNFTP)
+		await updateAdapters(eETH2X)
 
 		// Deploy new router
 		router = await deployFullRouter(
@@ -147,35 +209,6 @@ describe('Live Estimates', function () {
 		console.log('Withdraw Gas Used: ', receipt.gasUsed.toString())
 		const wethAfter = await weth.balanceOf(accounts[1].address)
 		console.log('Actual withdraw amount: ', wethAfter.sub(wethBefore).toString())
-	})
-
-	it('Should update adapter', async function () {
-		// Impersonate manager
-		const managerAddress = await eDPI.manager()
-		await network.provider.request({
-			method: 'hardhat_impersonateAccount',
-			params: [managerAddress],
-		})
-		const manager = await ethers.getSigner(managerAddress)
-
-		const items = await eDPI.items()
-		for (let i = 0; i < items.length; i++) {
-			 let tradeData = await eDPI.getTradeData(items[i])
-			 let adapters = [...tradeData.adapters]
-			 let shouldUpdate = false;
-			 for (let j = 0; j < adapters.length; j++) {
-				 	if (adapters[j].toLowerCase() == oldUniswapV3Address.toLowerCase()) {
-							adapters[j] = newUniswapV3Address
-							shouldUpdate = true;
-					}
-			 }
-			 if (shouldUpdate) {
-				 await eDPI.connect(manager).updateTradeData(items[i], {
-					 ...tradeData,
-					 adapters: adapters
-				 })
-			 }
-		}
 	})
 
 	it('Should estimate deposit eDPI', async function () {
