f4d8036b1997e5f691b3ab553b1bf742261963b9
George Carder <georgercarder@gmail.com>
2022-08-10 11:03:35 -0700

    reset interface

--- contracts/interfaces/IStrategyToken.sol
+++ contracts/interfaces/IStrategyToken.sol
@@ -4,8 +4,6 @@ pragma solidity >=0.6.0 <0.9.0;
 import "./IERC20NonStandard.sol";
 
 interface IStrategyToken is IERC20NonStandard {
-//    event FlashFeeUpdated(address account, uint256 fee);
-
     function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
 
     function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
@@ -19,11 +17,7 @@ interface IStrategyToken is IERC20NonStandard {
         bytes32 r,
         bytes32 s
     ) external;
-
- //   function setFlashFee(uint256 fee) external;
-
-  //  function flashTransfer(address from, address to, uint256 amount, bytes memory data) external;
-
+    
     function nonces(address owner) external view returns (uint256);
 
     function version() external view returns (string memory);
