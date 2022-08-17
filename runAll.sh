64aa92ec2edbf9c24175f1a1d026c2ec3fbbeeae
George Carder <georgercarder@gmail.com>
2022-08-17 15:50:30 -0700

    automate debugging workflow

--- runAll.sh
+++ runAll.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# npx hardhat run scripts/run-code-4rena-deploy.ts && \
-## TODO then manually change deployments.json
+git checkout deployments.json
+
+rm -r data && cp -r /home/george/synced/synced/enso/crawler-token-farms/data .
+
+npx hardhat run scripts/run-code-4rena-deploy.ts 
+
+_localhost=$(cat deployments.json | jq '.localhost')
+_mainnet=$(cat deployments.json | jq '.mainnet')
+cat deployments.json | jq ".mainnet=$_localhost" | jq "._mainnet=$_mainnet" | tee deployments.json
   npx hardhat run scripts/register/register_tokens.ts && \
   npx hardhat run scripts/register/register_uniswap_pools.ts && \
   npx hardhat run scripts/register/register_curve_pools.ts && \
