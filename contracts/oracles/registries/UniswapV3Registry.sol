005f022982dd67a7e98ce6329fb0cfe4d914b21a
George Carder <georgercarder@gmail.com>
2022-08-18 16:07:50 -0700

    log when both addresses are 0

--- contracts/oracles/registries/UniswapV3Registry.sol
+++ contracts/oracles/registries/UniswapV3Registry.sol
@@ -167,7 +167,8 @@ contract UniswapV3Registry is IUniswapV3Registry, StringUtils, Ownable {
 
     function _revertWith(string memory _msg, address a, address b) private view {
         if (b != address(0)) revert(string(abi.encodePacked(_msg, " ", toHexString(uint256(a), 20), " ", toHexString(uint256(b), 20))));
-        revert(string(abi.encodePacked(_msg, " ", toHexString(uint256(a), 20))));
+        if (a != address(0)) revert(string(abi.encodePacked(_msg, " ", toHexString(uint256(a), 20))));
+        revert(string(abi.encodePacked(_msg, " address(0).")));
     }
 
 }
